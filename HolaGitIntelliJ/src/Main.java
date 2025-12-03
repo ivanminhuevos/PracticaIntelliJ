56d981523a61a62b1907af8355b6f70f3e1daf78
Ivan <ivaminper@alu.edu.gva.es>
2025-12-03 10:07:58 +0100

    Añadido metodo mostrarSaludo en la rama feature-saludo

--- HolaGitIntelliJ/src/Main.java
+++ HolaGitIntelliJ/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+    public static void mostrarSaludo(String nombre) {
+        System.out.printf("Hola, %s! Bienvenido al projecto.\n", nombre);
+    }
+
     public static void main(String[] args) {
         System.out.println("Hola desde IntelliJ y Git!");
+
+        mostrarSaludo("Ivan");
     }
 }
