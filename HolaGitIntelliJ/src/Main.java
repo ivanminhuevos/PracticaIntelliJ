ea0f4ef123f5d98f9b9d1af30f7a9970a20bac69
Ivan <ivaminper@alu.edu.gva.es>
2025-12-03 10:13:12 +0100

    se me olvido comittear

--- HolaGitIntelliJ/src/Main.java
+++ HolaGitIntelliJ/src/Main.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scn = new Scanner(System.in);
+
     public static void mostrarSaludo(String nombre) {
         System.out.printf("Hola, %s! Bienvenido al projecto.\n", nombre);
+    }
+
+    private static String preguntarPorNombre() {
+        System.out.print("Cual es tu nombre? >");
+
+        return scn.nextLine();
     }
 
     public static void main(String[] args) {
         System.out.println("Hola desde IntelliJ y Git!");
 
-        mostrarSaludo("Ivan");
+        String nombre = preguntarPorNombre();
+
+        mostrarSaludo(nombre);
     }
 }
